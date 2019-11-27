3a8ebb3ae3ef291db0ada9073eeea0005e07742f
melia miller <meliajm@gmail.com>
2019-11-27 05:54:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,9 +11,26 @@ def map(array)
 end
 
 def reduce(array, option=nil)
-  
+  index = 0 
+  result = 
+  while index < array.length do 
+    
 end 
 
+
+def reduce_to_all_true(source_array)
+  index = 0 
+  result = true 
+  while index < source_array.length do 
+    if source_array[index]
+      result
+    else
+      result = false
+    end
+  index += 1  
+  end
+  result
+end
 
 # map_to_negative(x) {|n| n * -1}
 
