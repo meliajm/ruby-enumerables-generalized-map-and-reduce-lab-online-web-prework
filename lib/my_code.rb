64b0b8c9d38111ebab561a294b4f0d4ef009b57a
melia miller <meliajm@gmail.com>
2019-11-27 05:21:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+# map_to_negative(x) {|n| n * -1}
+
+require 'pry'
+
+def map(source_array) #block?
+  new = []
+  i = 0 
+  while i < source_array.length do 
+    new.push(source_array[i])
+    i += 1 
+  end
+  yield(new)
+end 
+
+# map([1,2,3], -1) do |innards|
+#   #binding.pry
+#   array = innards * -1
+#   array
+# end
+
+# array.map{ |num| num * -1 }
